7cc7ace6639f88f4efa3a3d53b67d3270b93191b
Yannik Sahl <s0569340@htw-berlin.de>
2019-11-15 17:08:31 +0100

    Added "CommonInterfaces"-Library Added methods to interfaces

---
+++ CodeGenerator/CommonInterfaces/IController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonInterfaces
+{
+    interface IController
+    {
+        bool startProcess(string filePath_Model, string filePath_Output);
+    }
+}
---
+++ CodeGenerator/CommonInterfaces/IGenerator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CodeGenerator.Reader;
+
+namespace CommonInterfaces
+{
+    interface IGenerator
+    {
+        bool generateCode(string filePath, Datamodel dml);
+
+    }
+}
