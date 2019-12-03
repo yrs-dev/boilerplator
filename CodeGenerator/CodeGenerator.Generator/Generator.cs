4319b625360a7abeb33b701468e8c1fed5ae2c20
Yannik Sahl <yanniksahl@outlook.de>
2019-12-03 16:39:16 +0100

    Added structure to Generator

--- CodeGenerator/CodeGenerator.Generator/Generator.cs
+++ CodeGenerator/CodeGenerator.Generator/Generator.cs
@@ -10,8 +10,22 @@ namespace CodeGenerator.Generator
     public class Generator : IGenerator
     {
 
-        bool generateCode(string filePath, Datamodel dml)
+        public bool generateCode(string filePath, CodeGenerator.Reader.Datamodel dml)
         {
+
+            // Get output location
+
+
+            // Iterate over data model
+
+
+            // Write class
+
+
+            // Write class attributes
+
+
+            // Write class methods
 
 
 
