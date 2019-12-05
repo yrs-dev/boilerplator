c013d0377e34582b575bb42c10de18bf66b844a8
Yannik Sahl <yanniksahl@outlook.de>
2019-12-05 09:45:59 +0100

    Reflected datamodel structure changes in the generator file

--- CodeGenerator/CommonInterfaces/IGenerator.cs
+++ CodeGenerator/CommonInterfaces/IGenerator.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using CodeGenerator.Reader;
+using CodeGenerator.Datamodel;
 
 namespace CommonInterfaces
 {
