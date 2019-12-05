19ee7a7c13896c504acf8a909270e6a184adb6bb
Baris Tikir <brstkr@users.noreply.github.com>
2019-12-05 20:29:48 +0100

    Refactored the Attribute section from the ReaderTest component

--- CodeGenerator/CodeGenerator.ReaderTest/Program.cs
+++ CodeGenerator/CodeGenerator.ReaderTest/Program.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using System.Xml;
 using System.Xml.Linq;
 using CodeGenerator.Reader;
+using CodeGenerator.Datamodel;
 
 namespace CodeGenerator.ReaderTest
 {
@@ -21,13 +22,19 @@ namespace CodeGenerator.ReaderTest
                 reader = new XmlTextReader(filepath);
                 while (reader.Read())
                 {
+                    CodeGenerator.Datamodel.Datamodel data = new CodeGenerator.Datamodel.Datamodel();
+                    UML_Class readClass = new UML_Class();
+                    UML_Attribute readAttribute = new UML_Attribute();
+
                     if (reader.Name == "y:NodeLabel" && reader.NodeType == XmlNodeType.Element)
                     {
                         Console.WriteLine("Class Name : {0}", CodeGenerator.Reader.Reader.getValue(reader.ReadSubtree()));
                     }
                     if (reader.Name == "y:AttributeLabel" && reader.NodeType == XmlNodeType.Element)
                     {
-                        Console.WriteLine("Attribute Names : {0}", CodeGenerator.Reader.Reader.getValue(reader.ReadSubtree()));
+                        string name = CodeGenerator.Reader.Reader.getValue(reader.ReadSubtree());
+                        Console.WriteLine("Attribute Name : {0}", name);
+                        readClass.name = name;
                     }
                     if (reader.Name == "y:MethodLabel" && reader.NodeType == XmlNodeType.Element)
                     {
