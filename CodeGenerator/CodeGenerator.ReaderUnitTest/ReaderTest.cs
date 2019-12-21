ffbb02b33db32f279fc0abae2936beedf9fbe851
Baris Tikir <brstkr@users.noreply.github.com>
2019-12-21 15:34:54 +0100

    Created first working unit test for "AnalyzeNodeLabel" Method of the Reader and also added needed graphml files for the test.

---
+++ CodeGenerator/CodeGenerator.ReaderUnitTest/ReaderTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using System.IO;
+using System.Xml;
+using CodeGenerator.Datamodel;
+
+namespace CodeGenerator.ReaderUnitTest
+{
+    
+    public class ReaderTest
+    {
+        [Fact]
+        public void CanGetValueofAnalyzeNodeLabel()
+        {
+            // Arange
+            string className = "";
+            string filepath = Environment.CurrentDirectory + "/classdiagram.graphml";
+
+            UML_Class ClassExpected = new UML_Class(className);
+            ClassExpected.name = "Employee";
+
+
+            // Act
+            XmlReader reader = new XmlTextReader(filepath);
+            UML_Class ClassActual = new UML_Class(className);
+            ClassActual = CodeGenerator.Reader.Reader.AnalyzeNodeLabel(reader);
+
+            // Assert
+            Assert.Equal(ClassExpected.name ,ClassActual.name);
+
+        }
+    }
+}
