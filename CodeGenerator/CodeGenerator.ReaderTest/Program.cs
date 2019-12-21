ea3b9ec42d8b37bb90207ed22a12c00b6ad9bf23
Baris Tikir <brstkr@users.noreply.github.com>
2019-12-21 14:59:31 +0100

    Deleted old Test for Reader Component

--- CodeGenerator/CodeGenerator.ReaderTest/Program.cs
+++ CodeGenerator/CodeGenerator.ReaderTest/Program.cs
@@ -12,39 +12,6 @@ namespace CodeGenerator.ReaderTest
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            string filepath = "";
-            XmlTextReader reader = null;
-
-            try
-            {
-                reader = new XmlTextReader(filepath);
-                while (reader.Read())
-                {
-                    CodeGenerator.Datamodel.Datamodel data = new CodeGenerator.Datamodel.Datamodel();
-                    UML_Class readClass = new UML_Class();
-                    UML_Attribute readAttribute = new UML_Attribute();
-
-                    if (reader.Name == "y:NodeLabel" && reader.NodeType == XmlNodeType.Element)
-                    {
-                        Console.WriteLine("Class Name : {0}", CodeGenerator.Reader.Reader.getValue(reader.ReadSubtree()));
-                    }
-                    if (reader.Name == "y:AttributeLabel" && reader.NodeType == XmlNodeType.Element)
-                    {
-                        string name = CodeGenerator.Reader.Reader.getValue(reader.ReadSubtree());
-                        Console.WriteLine("Attribute Name : {0}", name);
-                        readClass.name = name;
-                    }
-                    if (reader.Name == "y:MethodLabel" && reader.NodeType == XmlNodeType.Element)
-                    {
-                        Console.WriteLine("Method Names : {0}", CodeGenerator.Reader.Reader.getValue(reader.ReadSubtree()));
-                    }
-                }
-            }
-            finally
-            {
-            }
-        }
+   
     }
 }
