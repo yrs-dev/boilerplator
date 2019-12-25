a5dc5d905d1d742d73ac1b491adfd71e0e2bead3
Baris Tikir <brstkr@users.noreply.github.com>
2019-12-25 22:39:26 +0100

    Implemented unit test for "AnalyzeMethodLabel"

--- CodeGenerator/CodeGenerator.ReaderUnitTest/ReaderTest.cs
+++ CodeGenerator/CodeGenerator.ReaderUnitTest/ReaderTest.cs
@@ -63,5 +63,57 @@ namespace CodeGenerator.ReaderUnitTest
             // Assert
             Assert.Equal(expectedAttributes, classAttributes);
         }
+
+        [Fact]
+        public void CanGetValueOfAnalyzeMethodLabel()
+        {
+            // Arrange
+            string filepath = Environment.CurrentDirectory + "/classdiagram.graphml";
+
+            List<UML_Method> expectedMethods = new List<UML_Method>();
+            UML_Method method1 = new UML_Method()
+            {
+                name = "getName",
+                type = "String",
+            };
+            UML_Method method2 = new UML_Method()
+            {
+
+                name = "getTitle",
+                type = "String"
+            };
+            UML_Method method3 = new UML_Method()
+            {
+
+                name = "getStaffNo",
+                type = "Number"
+            };
+            UML_Method method4 = new UML_Method()
+            {
+
+                name = "getRoom",
+                type = "String"
+            };
+            UML_Method method5 = new UML_Method()
+            {
+
+                name = "getPhone"
+            };
+
+            expectedMethods.Add(method1);
+            expectedMethods.Add(method2);
+            expectedMethods.Add(method3);
+            expectedMethods.Add(method4);
+            expectedMethods.Add(method5);
+
+            List<UML_Method> classMethods = new List<UML_Method>();
+
+            // Act
+            XmlReader reader = new XmlTextReader(filepath);
+            classMethods = CodeGenerator.Reader.Reader.AnalyzeMethodLabel(reader);
+
+            // Assert
+            Assert.Equal(expectedMethods, classMethods);
+        }
     }
 }
