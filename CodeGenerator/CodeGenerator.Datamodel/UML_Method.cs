f501469a53baa4508d4758f396a51c044015df6e
Baris Tikir <brstkr@users.noreply.github.com>
2019-12-26 23:22:01 +0100

    Overwritten the "Equals" method for functional unit testing

--- CodeGenerator/CodeGenerator.Datamodel/UML_Method.cs
+++ CodeGenerator/CodeGenerator.Datamodel/UML_Method.cs
@@ -11,5 +11,9 @@ namespace CodeGenerator.Datamodel
         // List to store method parameters, may be empty
         public List<UML_Parameter> parameters { get; set; }
 
+        public override bool Equals(object obj)
+        {
+            return this.accessModifier == ((UML_Method)obj).accessModifier && this.name == ((UML_Method)obj).name && this.type == ((UML_Method)obj).type && this.parameters == ((UML_Method)obj).parameters;
+        }
     }
 }
