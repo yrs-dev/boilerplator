20df4725f9407dcfeaec8ed1b4185c01f042f96d
Yannik Sahl <s0569340@htw-berlin.de>
2019-12-27 20:07:31 +0100

    Finalized ClassGenerator

--- CodeGenerator/CodeGenerator.Generator/ClassGenerator.cs
+++ CodeGenerator/CodeGenerator.Generator/ClassGenerator.cs
@@ -3,10 +3,166 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using CommonInterfaces;
+using CodeGenerator.Datamodel;
 
 namespace CodeGenerator.Generator
 {
+    /// <summary>  
+    /// Class responsible for generating UML classes. 
+    /// </summary> 
     public class ClassGenerator
     {
+        /// <summary> Variable to store the currently used file. </summary> 
+        private StreamWriter classFile;
+
+        /// <summary> Variable which will contain the needed information for the class generator logic. </summary> 
+        private UML_Class umlClass;
+
+
+        /// <summary>  
+        /// Constructor for the class. Takes the current file and the data object as input.
+        /// </summary> 
+        public ClassGenerator(StreamWriter classFile, UML_Class umlClass)
+        {
+            this.classFile = classFile;
+            this.umlClass = umlClass;
+        }
+
+
+
+        /// <summary>
+        /// Top-method orchestrating the generator steps.
+        /// </summary>
+        public void generateClass()
+        {
+            // Write class
+            writeClass();
+
+            // Write attributes
+            writeAttributes();
+
+            // Write methods
+            writeMethods();
+
+            // Ending line
+            classFile.WriteLine("}");
+        }
+
+
+        /// <summary>
+        /// Writes the introductory class line with the name matching what is found in the UML_Class object.
+        /// </summary>
+        void writeClass()
+        {
+            // Write beginning
+            StringBuilder sb = new StringBuilder($"class {umlClass.name}");
+
+            // Append parents, interfaces
+            writeClass_appendElements(sb);
+
+            // Last line
+            sb.Append(")\n{");
+
+            // Write built string
+            classFile.WriteLine(sb.ToString());
+        }
+
+
+
+        /// <summary>
+        /// Writes elements belonging to the class declaration that may or may not be neccessary (e.g. parent classes or interfaces)
+        /// </summary>
+        /// <param name="sb">Currently used StringBuilder.</param>
+        void writeClass_appendElements(StringBuilder sb)
+        {
+
+            // Append colon
+            bool aditionalElementsPresent = umlClass.parents.Count > 0 || umlClass.implementedInterfaces.Count > 0;
+            if (aditionalElementsPresent)
+            {
+                sb.Append(" :");
+            }
+
+            // Append parents
+            if (umlClass.parents.Count > 0)
+            {
+                foreach (UML_Class parentClass in umlClass.parents)
+                {
+                    sb.Append($"{parentClass.name}, ");
+                }
+            }
+
+            // Append interfaces
+            if (umlClass.implementedInterfaces.Count > 0)
+            {
+                foreach (UML_Interface implementedInterface in umlClass.implementedInterfaces)
+                {
+                    sb.Append($"{implementedInterface.name}, ");
+                }
+            }
+
+            // Delete last comma
+            if (aditionalElementsPresent)
+            {
+                sb.Length = sb.Length - 2;
+            }
+        }
+
+
+        /// <summary>
+        /// Writes attributes into a specified file with the name and type matching what is found in the passed list of attributes that belong to the current class.
+        /// </summary>
+        void writeAttributes()
+        {
+
+            // Extract attributes
+            List<UML_Attribute> umlAttributes = umlClass.umlAttributes;
+
+            // Iterate over attribute list
+            foreach (UML_Attribute umlAttribute in umlAttributes)
+            {
+                // Write attribute to file
+                string attributeString = $"{umlAttribute.accessModifier} {umlAttribute.type} {umlAttribute.name};";
+                classFile.WriteLine(attributeString);
+            }
+
+        }
+
+
+        /// <summary>
+        /// Writes empty functions into a specified file with the name and parameters matching what is found in the passed list of methods that belong to the current class.
+        /// </summary>
+        void writeMethods()
+        {
+
+            // Extract methods
+            List<UML_Method> umlMethods = umlClass.umlMethods;
+
+            // Iterate over method list
+            foreach (UML_Method umlMethod in umlMethods)
+            {
+                StringBuilder sb = new StringBuilder();
+
+                // First line
+                sb.Append($"{umlMethod.accessModifier} {umlMethod.type} {umlMethod.name} (");
+                foreach (UML_Parameter umlParameter in umlMethod.parameters)
+                {
+                    sb.Append($"{umlParameter.parameterName} {umlParameter.parameterType}, ");
+                }
+
+                sb.Length = sb.Length - 2; // Delete trailing ,
+                sb.Append(")");
+
+                // Write body
+                sb.Append("\n{\n\tthrow new System.NotImplementedException();\n}");
+
+                // Write built string
+                classFile.WriteLine(sb.ToString());
+
+            }
+        }
+
     }
 }
--- CodeGenerator/CodeGenerator.Generator/Generator.cs
+++ CodeGenerator/CodeGenerator.Generator/Generator.cs
@@ -1,25 +1,22 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.IO;
 using CommonInterfaces;
 using CodeGenerator.Datamodel;
 
-/* TODO:
- * 
- * 
- * 
- * 
- */
+
+
 
 namespace CodeGenerator.Generator
 {
     public class Generator : IGenerator
     {
 
-        // Main method
+        /// <summary>
+        /// Writes .cs files according to the passed Datamodel to the specified file path.
+        /// </summary>
+        /// <param name="filePath">Created files go here.</param>
+        /// <param name="dml">Datamodel object containing information about everything found in the class diagram.</param>
+        /// <returns>Nothing.</returns>
         public bool generateCode(string filePath, Datamodel.Datamodel dml)
         {
 
@@ -30,20 +27,15 @@ namespace CodeGenerator.Generator
                 using (StreamWriter classFile = new StreamWriter(filePath + umlClass.name + ".cs"))
                 {
 
-                    // Write class
-                    writeClass(classFile, umlClass);
+                    // Create ClassGenerator object
+                    ClassGenerator cGen = new ClassGenerator(classFile, umlClass);
 
-                    // Write attributes
-                    writeAttribute(classFile, umlClass.umlAttributes);
-
-                    // Write methods
-                    writeMethod(classFile, umlClass.umlMethods);
-
-                    // Ending line
-                    classFile.WriteLine("}");
+                    // Generate class
+                    cGen.generateClass();
 
                 }
             }
+
             // Iterate over interfaces
             foreach (UML_Interface umlInterface in dml.umlInterfaces)
             {
@@ -51,153 +43,17 @@ namespace CodeGenerator.Generator
                 using (StreamWriter interfaceFile = new StreamWriter(filePath + umlInterface.name + ".cs"))
                 {
 
-                    // Write class
-                    writeClass(classFile, umlClass);
+                    // Create InterfaceGenerator object
+                    InterfaceGenerator iGen = new InterfaceGenerator(classFile, umlClass);
 
-                    // Write attributes
-                    writeAttribute(classFile, umlClass.umlAttributes);
-
-                    // Write methods
-                    writeMethod(classFile, umlClass.umlMethods);
-
-                    // Ending line
-                    classFile.WriteLine("}");
+                    // Generate class
+                    iGen.generateInterface();
 
                 }
             }
 
              return true;
         }
-
-
-
-        /// <summary>
-        /// Writes the introductory class line with the name matching what is found in the UML_Class object.
-        /// </summary>
-        /// <param name="classFile">File to be written into.</param>
-        /// <param name="umlClass">UML_Class object containing information about to be written class.</param>
-        /// <returns>Nothing.</returns>
-        void writeClass(StreamWriter classFile, UML_Class umlClass)
-        {
-            // Write beginning
-            StringBuilder sb = new StringBuilder($"class {umlClass.name}");
-
-            // Append parents, interfaces
-            writeClass_appendElements(classFile, umlClass, sb);
-
-            // Last line
-            sb.Append(")\n{");
-
-            // Write built string
-            classFile.WriteLine(sb.ToString());
-        }
-
-
-
-        /// <summary>
-        /// Writes elements belonging to the class declaration that may or may not be neccessary (e.g. parent classes or interfaces)
-        /// </summary>
-        /// <param name="classFile">File to be written into.</param>
-        /// <param name="umlClass">UML_Class object containing information about to be written class.</param>
-        /// <param name="sb">Currently used StringBuilder.</param>
-        void writeClass_appendElements(StreamWriter classFile, UML_Class umlClass, StringBuilder sb)
-        {
-
-            // Append colon
-            bool aditionalElementsPresent = umlClass.parents.Count > 0 || umlClass.implementedInterfaces.Count > 0;
-            if (aditionalElementsPresent)
-            {
-                sb.Append(" :");
-            }
-
-            // Append parents
-            if (umlClass.parents.Count > 0)
-            {
-                foreach (UML_Class parentClass in umlClass.parents)
-                {
-                    sb.Append($"{parentClass.name}, ");
-                }
-            }
-
-            // Append interfaces
-            if (umlClass.implementedInterfaces.Count > 0)
-            {
-                foreach (UML_Interface implementedInterface in umlClass.implementedInterfaces)
-                {
-                    sb.Append($"{implementedInterface.name}, ");
-                }
-            }
-
-            // Delete last comma
-            if (aditionalElementsPresent)
-            {
-                sb.Length = sb.Length - 2;
-            }
-        }
-
-
-        /// <summary>
-        /// Writes attributes into a specified file with the name and type matching what is found in the passed list of attributes that belong to the current class.
-        /// </summary>
-        /// <param name="classFile">File to be written into.</param>
-        /// <param name="umlAttributes">List of all attributes found in the Class Diagram.</param>
-        /// <returns>Nothing.</returns>
-        void writeAttribute(StreamWriter classFile, List<UML_Attribute> umlAttributes)
-        {
-
-            // Iterate over attribute list
-            foreach(UML_Attribute umlAttribute in umlAttributes)
-            {
-                // Write attribute to file
-                string attributeString = $"{umlAttribute.accessModifier} {umlAttribute.type} {umlAttribute.name};";
-                classFile.WriteLine(attributeString);
-            }
-            
-        }
-
-
-        /// <summary>
-        /// Writes empty functions into a specified file with the name and parameters matching what is found in the passed list of methods that belong to the current class.
-        /// </summary>
-        /// <param name="classFile">File to be written into.</param>
-        /// <param name="umlMethods">List of all methods found in the Class Diagram.</param>
-        /// <returns>Nothing.</returns>
-        void writeMethod(StreamWriter classFile, List<UML_Method> umlMethods)
-        {
-
-            // Iterate over method list
-            foreach (UML_Method umlMethod in umlMethods)
-            {
-                StringBuilder sb = new StringBuilder();
-
-                // First line
-                sb.Append($"{umlMethod.accessModifier} {umlMethod.type} {umlMethod.name} (");
-                foreach (UML_Parameter umlParameter in umlMethod.parameters)
-                {
-                    sb.Append($"{umlParameter.parameterName} {umlParameter.parameterType}, ");
-                }
-
-                sb.Length = sb.Length - 2; // Delete trailing ,
-                sb.Append(")");
-
-                // Write body
-                sb.Append("\n{\n\tthrow new System.NotImplementedException();\n}");
-
-                // Write built string
-                classFile.WriteLine(sb.ToString());
-
-            }
-        }
-
-        
-
-        void testMethod(Type UML_Class)
-        {
-            
-        }
-
-
-
 
     }
 }
