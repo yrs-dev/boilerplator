69900f8a42c3501a13e35033fa49baf2d88e478a
Yannik Sahl <yanniksahl@outlook.de>
2019-12-31 10:38:26 +0100

    Proposal: move parameters from "generateCode(...)" method to constructor because object "needs" them (for other methods)

--- CodeGenerator/CodeGenerator.Generator/Generator.cs
+++ CodeGenerator/CodeGenerator.Generator/Generator.cs
@@ -2,7 +2,7 @@
 using System.IO;
 using CommonInterfaces;
 using CodeGenerator.Datamodel;
-using System.Linq;
+using dm = CodeGenerator.Datamodel;
 
 /* TODO: 
  * bool error return
@@ -16,14 +16,36 @@ namespace CodeGenerator.Generator
     public class Generator : IGenerator
     {
 
+        /*
+        /// <summary> Created files go here. </summary>
+        public string filePath { get; set; }
+
+        /// <summary> Datamodel object containing information about everything found in the class diagram.</summary>
+        public dm.Datamodel dml { get; set; }
+
+
+        // Constructor
+        public Generator(string filePath, dm.Datamodel dml)
+        {
+            this.filePath = filePath;
+            this.dml = dml;
+        }
+        */
+
         /// <summary>
         /// Writes ".cs" files to the specified file path according to the passed Datamodel.
         /// </summary>
-        /// <param name="filePath">Created files go here.</param>
-        /// <param name="dml">Datamodel object containing information about everything found in the class diagram.</param>
+        /// <param name="filePath">Created files go here. </param>
+        /// <param name="dml">Datamodel object containing information about everything found in the class diagram. </param>
         /// <returns>Nothing.</returns>
-        public bool generateCode(string filePath, Datamodel.Datamodel dml)
+        public bool generateCode(string filePath, dm.Datamodel dml)
         {
+
+            // Check datamodel
+            if (!isDataModelComplete())
+            {
+                return false;
+            }
 
             // Iterate over classes
             foreach (UML_Class umlClass in dml.umlClasses)
@@ -57,7 +79,14 @@ namespace CodeGenerator.Generator
                 }
             }
 
-             return true;
+            return true;
+        }
+
+        private bool isDataModelComplete()
+        {
+
+
+            return false;
         }
 
     }
