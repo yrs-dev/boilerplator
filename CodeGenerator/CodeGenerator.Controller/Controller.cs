c713b60f724c45be4cb501194f186242fab2503b
Emircan Yüksel <emir-00@live.de>
2020-01-04 01:30:12 +0100

    Basic Structure of Controller. Little Bugfixes.

--- CodeGenerator/CodeGenerator.Controller/Controller.cs
+++ CodeGenerator/CodeGenerator.Controller/Controller.cs
@@ -4,10 +4,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using CommonInterfaces;
+using CodeGenerator.GUI;
 
 namespace CodeGenerator.Controller
 {
     public class Controller : IController
     {
+        public bool IController.StartProcess(string filePath_Model, string filePath_Output)
+        {
+            
+        }
+
+        public bool checkPermission(string filePath){}
+
+        public bool exchangeData(){}
     }
 }
---
+++ CodeGenerator/CodeGenerator.Controller/GeneratorController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommonInterfaces;
+
+namespace CodeGenerator.Controller
+{
+    public class GeneratorController : Controller
+    {
+
+    }
+}
