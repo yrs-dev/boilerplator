2fec460d3587f7064e3995ea84b2f14dd6252a49
Emircan Yüksel <emir-00@live.de>
2020-01-16 20:25:04 +0100

    Final ControllerTest.

--- CodeGenerator/CodeGenerator.ControllerTest/Program.cs
+++ CodeGenerator/CodeGenerator.ControllerTest/Program.cs
@@ -12,13 +12,13 @@ namespace CodeGenerator.ControllerTest
         {
             string filePath_Output = Environment.CurrentDirectory;
             int Index = filePath_Output.IndexOf("/Program");
-            string filePath_Model = filePath_Output.Substring(0, Index);
+            string filePath_Model = filePath_Output.Substring(0, Index) + "/classdiagram.cs";
             StartProcess(filePath_Model, filePath_Output);
-
         }
 
         private static bool StartProcess(string filePath_Model, string filePath_Output)
-        {Reader.Reader reader = new Reader.Reader(filePath_Model);
+        {
+            Reader.Reader reader = new Reader.Reader(filePath_Model);
             Datamodel.Datamodel datamodel = reader.getDatamodel();
             Generator.Generator generator = new Generator.Generator(filePath_Output, datamodel);
             return generator.generateCode();
